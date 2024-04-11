d3f103b1a5adbf2860e7fcceab7cccd1eec6bd76
Cbass92 <140918855+sebastian-92@users.noreply.github.com>
2024-04-11 09:17:00 -0400

    Poki sdk crack

--- crossyroad/poki-sdk.js
+++ crossyroad/poki-sdk.js
@@ -1,1 +1,84 @@
-(()=>{var e=function(e){var n=RegExp("[?&]"+e+"=([^&]*)").exec(window.location.search);return n&&decodeURIComponent(n[1].replace(/\+/g," "))},n="kids"===e("tag"),o=new(function(){function e(){var e=this;this.queue=[],this.init=function(n){return void 0===n&&(n={}),new Promise((function(o,t){e.enqueue("init",n,o,t)}))},this.rewardedBreak=function(){return new Promise((function(e){e(!1)}))},this.noArguments=function(n){return function(){e.enqueue(n)}},this.oneArgument=function(n){return function(o){e.enqueue(n,o)}},this.handleAutoResolvePromise=function(){return new Promise((function(e){e()}))},this.handleAutoResolvePromiseObj=function(){return new Promise((function(e){e()}))},this.throwNotLoaded=function(){console.debug("PokiSDK is not loaded yet. Not all methods are available.")}}return e.prototype.enqueue=function(e,o,t,i){var r={fn:e,options:o,resolveFn:t,rejectFn:i};n?i&&i():this.queue.push(r)},e.prototype.dequeue=function(){for(var e=function(){var e=n.queue.shift(),o=e,t=o.fn,i=o.options;"function"==typeof window.PokiSDK[t]?(null==e?void 0:e.resolveFn)||(null==e?void 0:e.rejectFn)?window.PokiSDK[t](i).then((function(){for(var n=[],o=0;o<arguments.length;o++)n[o]=arguments[o];"function"==typeof e.resolveFn&&e.resolveFn.apply(e,n)})).catch((function(){for(var n=[],o=0;o<arguments.length;o++)n[o]=arguments[o];"function"==typeof e.rejectFn&&e.rejectFn.apply(e,n)})):void 0!==(null==e?void 0:e.fn)&&window.PokiSDK[t](i):console.error("Cannot execute "+e.fn)},n=this;this.queue.length>0;)e()},e}());window.PokiSDK={init:o.init,initWithVideoHB:o.init,customEvent:o.throwNotLoaded,destroyAd:o.throwNotLoaded,getLeaderboard:o.handleAutoResolvePromiseObj},["disableProgrammatic","gameLoadingStart","gameLoadingFinished","gameInteractive","roundStart","roundEnd","muteAd"].forEach((function(e){window.PokiSDK[e]=o.noArguments(e)})),["setDebug","gameplayStart","gameplayStop","gameLoadingProgress","happyTime","setPlayerAge","togglePlayerAdvertisingConsent","toggleNonPersonalized","setConsentString","logError","sendHighscore","setDebugTouchOverlayController"].forEach((function(e){window.PokiSDK[e]=o.oneArgument(e)}));var t,i=((t=window.pokiSDKVersion)||(t=e("ab")||"v2.234.2"),"poki-sdk-"+(n?"kids":"core")+"-"+t+".js"),r=document.createElement("script");r.setAttribute("src",i),r.setAttribute("type","text/javascript"),r.setAttribute("crossOrigin","anonymous"),r.onload=function(){return o.dequeue()},document.head.appendChild(r)})();
+(() => {
+    var getParameterByName = function(name) {
+        var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+        return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+    };
+
+    var isKidsTag = "kids" === getParameterByName("tag");
+
+    var SDK = new (function() {
+        var self = this;
+
+        this.queue = [];
+
+        this.init = function(options) {
+            return new Promise(function(resolve, reject) {
+                self.enqueue("init", options, resolve, reject);
+            });
+        };
+
+        this.rewardedBreak = function() {
+            return new Promise(function(resolve) {
+                // Grant rewards immediately without checking for ad views
+                resolve(true);
+            });
+        };
+
+        this.noArguments = function(name) {
+            return function() {
+                self.enqueue(name);
+            };
+        };
+
+        this.oneArgument = function(name) {
+            return function(arg) {
+                self.enqueue(name, arg);
+            };
+        };
+
+        this.handleAutoResolvePromise = function() {
+            return new Promise(function(resolve) {
+                resolve();
+            });
+        };
+
+        this.handleAutoResolvePromiseObj = function() {
+            return new Promise(function(resolve) {
+                resolve();
+            });
+        };
+
+        this.throwNotLoaded = function() {
+            console.debug("PokiSDK is not loaded yet. Not all methods are available.");
+        };
+    })();
+
+    window.PokiSDK = {
+        init: SDK.init,
+        initWithVideoHB: SDK.init,
+        customEvent: SDK.throwNotLoaded,
+        destroyAd: SDK.throwNotLoaded,
+        getLeaderboard: SDK.handleAutoResolvePromiseObj
+    };
+
+    ["disableProgrammatic", "gameLoadingStart", "gameLoadingFinished", "gameInteractive", "roundStart", "roundEnd", "muteAd"].forEach(function(name) {
+        window.PokiSDK[name] = SDK.noArguments(name);
+    });
+
+    ["setDebug", "gameplayStart", "gameplayStop", "gameLoadingProgress", "happyTime", "setPlayerAge", "togglePlayerAdvertisingConsent", "toggleNonPersonalized", "setConsentString", "logError", "sendHighscore", "setDebugTouchOverlayController"].forEach(function(name) {
+        window.PokiSDK[name] = SDK.oneArgument(name);
+    });
+
+    var sdkVersion = window.pokiSDKVersion || getParameterByName("ab") || "v2.234.2";
+    var scriptSrc = "poki-sdk-" + (isKidsTag ? "kids" : "core") + "-" + sdkVersion + ".js";
+    var scriptElement = document.createElement("script");
+
+    scriptElement.setAttribute("src", scriptSrc);
+    scriptElement.setAttribute("type", "text/javascript");
+    scriptElement.setAttribute("crossOrigin", "anonymous");
+    scriptElement.onload = function() {
+        return SDK.dequeue();
+    };
+
+    document.head.appendChild(scriptElement);
+})();
